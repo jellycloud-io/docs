8a63dbd7650f68d1f641256ff19a384bb652be79
Arthur Averbuch <arthur@jellycloud.io>
2026-04-20 13:46:27 +0300

    add Google Analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,6 +70,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-721ZLR0KJW',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
